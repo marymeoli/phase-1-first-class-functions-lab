9b1ffd0d3ee25768fca200172dae20eac5ff90cb
Moringa School <marymeoli77@gmail.com>
2022-09-26 10:54:27 +0300

    complete assigment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers =(driverArr) => {return driverArr.slice(0,2)}
+const returnLastTwoDrivers = (driverArr) => {return driverArr.slice(-2)};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = (ride) => {
+    return (fare) => {
+        return ride * fare;
+    }
+}
+const fareDoubler = (fare) => {return fare *2}
+const fareTripler = (fare) => {return fare *3}
+const selectDifferentDrivers = (driver,func) => { return func(driver)}
